549b7893c222899997249f0707e4a18a44277b20
oltapllana <oltapllana@gmail.com>
2025-01-21 04:26:46 +0100

    first commit

--- src/Adoption.jsx
+++ src/Adoption.jsx
@@ -9,7 +9,7 @@ const [loading, setLoading] = useState(false)
   useEffect(() => {
     async  function adopt(){
       setLoading(true)
-      const url = `https://api-staging.adoptapet.com/search/pet_search?key=hg4nsv85lppeoqqixy3tnlt3k8lj6o0c&v=3&output=json&city_or_zip=47374&geo_range=50&species=dog&breed_id=real=801&sex=m&age=young&color_id=54&pet_size_range_id=2&hair=&bonded_pair=&special_needs=&include_mixes=&added_after=&start_number=1&end_number=50&meta_only=0`
+      const url = `https://api-staging.adoptapet.com/search/pet_search?key=${import.meta.env.VITE_API_KEY}&output=json&city_or_zip=47374&geo_range=50&species=dog&breed_id=real=801&sex=m&age=young&color_id=54&pet_size_range_id=2&hair=&bonded_pair=&special_needs=&include_mixes=&added_after=&start_number=1&end_number=50&meta_only=0`
       const response = await fetch(url);
       const data = await response.json();
      setAdoptionList(data.pets.slice(0,15))
