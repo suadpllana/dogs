62b886450a40eb9ef5f2510519144286b3a604c5
oltapllana <oltapllana@gmail.com>
2025-02-05 00:28:14 +0100

    first commit

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import {useState} from "react"
+import { TiThMenu } from "react-icons/ti";
 const Nav = () => {
+  const [isMenuOpen,setIsMenuOpen] = useState(false);
+
   return (
 
        <nav className="navContainer">
-        <Link to="/dogs/">Home</Link>
-        <Link to="/dogs/adoption">Adoption</Link>
-        <Link to="/dogs/breed">Breed</Link>
-        <Link to="/dogs/facts">Facts</Link>
+                 <TiThMenu onClick={() => setIsMenuOpen(!isMenuOpen)} className="menu"/>
+
+
+
+        <div className={isMenuOpen ? "links open" : "links"}>
+ 
+        <Link onClick={() => setIsMenuOpen(false)} to="/dogs/">Home</Link>
+        <Link onClick={() => setIsMenuOpen(false)} to="/dogs/adoption">Adoption</Link>
+        <Link onClick={() => setIsMenuOpen(false)} to="/dogs/breed">Breed</Link>
+        <Link onClick={() => setIsMenuOpen(false)} to="/dogs/facts">Facts</Link>
+        </div>
+      
     </nav>
    
   )
